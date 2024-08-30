4a114806af613d9efd6cacdee0f8eadf9c7e8902
MrRamyg <153380674+MrRamyg@users.noreply.github.com>
2024-08-30 13:37:46 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,7 +3,6 @@
 # -- Project information -----------------------------------------------------
 
 project = 'Lumache'
-copyright = '2021, Graziella'
 author = 'Graziella'
 
 release = '0.1'
@@ -17,7 +16,12 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
 ]
+
+templates_path = ['_templates']
+exclude_patterns = []
 
 # Mapping for intersphinx (links to other documentation)
 intersphinx_mapping = {
@@ -35,9 +39,12 @@ source_suffix = '.rst'
 # Master document (source file without extension)
 master_doc = 'index'
 
+# Source directory
+source_dir = 'docs/source'
+
 # -- Options for HTML output -------------------------------------------------
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_wagtail_theme'
 
 # -- Options for EPUB output -------------------------------------------------
 epub_show_urls = 'footnote'
