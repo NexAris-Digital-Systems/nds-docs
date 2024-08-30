17b1d56af4a8626aa53749b0e3c82c10865a0887
MrRamyg <153380674+MrRamyg@users.noreply.github.com>
2024-08-30 15:49:06 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -27,6 +27,15 @@ html_css_files = [
     'custom.css',
 ]
 
+html_theme_options = dict(
+    project_name = "Crazy Development",
+    logo = "https://cdn.crazydev.org/_data/i/upload/2024/08/29/20240829135605-1e4ae887-th.png",
+    logo_alt = "crazydev",
+    logo_height = 59,
+    logo_url = "/",
+    logo_width = 45,
+)
+
 # -- Options for HTML output -------------------------------------------------
 extensions.append("sphinx_wagtail_theme")
 html_theme = 'sphinx_wagtail_theme'
