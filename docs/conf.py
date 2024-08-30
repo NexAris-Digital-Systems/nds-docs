ce5e01ad0fec246500395ccb899d6040f866dab8
MrRamyg <153380674+MrRamyg@users.noreply.github.com>
2024-08-30 12:52:12 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,4 @@
-import os
-import sys
-
-# Add the path to the source files to sys.path
-sys.path.insert(0, os.path.abspath('../source'))
+# Configuration file for the Sphinx documentation builder.
 
 # -- Project information -----------------------------------------------------
 
@@ -41,7 +37,7 @@ master_doc = 'index'
 
 # -- Options for HTML output -------------------------------------------------
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 
 # -- Options for EPUB output -------------------------------------------------
 epub_show_urls = 'footnote'
