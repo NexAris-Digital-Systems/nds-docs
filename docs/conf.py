7094bbf5f29ecc1bfa9d300da78b203b0000490b
MrRamyg <153380674+MrRamyg@users.noreply.github.com>
2024-08-30 14:36:42 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,50 +1,33 @@
-# Configuration file for the Sphinx documentation builder.
-
 # -- Project information -----------------------------------------------------
-
-project = 'Crazy Development Docs'
+project = 'Crazy Development'
 author = 'MrRamyg'
-
 release = '0.1'
 version = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
-
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
-    'recommonmark',  
-    'sphinx_wagtail_theme',  
+    'recommonmark', 
+    'sphinx_markdown_tables', 
 ]
 
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
+
+master_doc = 'index'
 templates_path = ['_templates']
 exclude_patterns = []
 
-# Mapping for intersphinx (links to other documentation)
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-intersphinx_disabled_domains = ['std']
-
-# Source file suffixes
-source_suffix = '.md'
-
-# Master document (source file without extension)
-master_doc = 'index'
-
-# Source directory
-source_dir = 'docs/source'
-
 # -- Options for HTML output -------------------------------------------------
-
 extensions.append("sphinx_wagtail_theme")
 html_theme = 'sphinx_wagtail_theme'
+
 
 # -- Options for EPUB output -------------------------------------------------
 epub_show_urls = 'footnote'
