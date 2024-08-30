2d2bf70603af189c9af54d4c11a05458b22cc84a
MrRamyg <153380674+MrRamyg@users.noreply.github.com>
2024-08-30 12:23:23 -0500

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,43 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information -----------------------------------------------------
+
+project = 'Lumache'
+copyright = '2021, Graziella'
+author = 'Graziella'
+
+release = '0.1'
+version = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+
+# Mapping for intersphinx (links to other documentation)
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+# Paths to templates
+templates_path = ['_templates']
+
+# Source file suffixes
+source_suffix = '.rst'
+
+# Master document (source file without extension)
+master_doc = 'index'
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'sphinx_rtd_theme'
+
+# -- Options for EPUB output -------------------------------------------------
+epub_show_urls = 'footnote'
