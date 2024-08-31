762771b216de2276e5053f3335f6668cc8c645e8
MrRamyg <153380674+MrRamyg@users.noreply.github.com>
2024-08-30 20:44:50 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -5,6 +5,7 @@ release = '0.1'
 version = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
+html_favicon = "https://cdn.crazydev.org/_data/i/upload/2024/08/14/20240814080422-a2794fa4-th.png"
 extensions = [
     'myst_parser',
     'sphinx.ext.autodoc',
@@ -41,7 +42,7 @@ html_last_updated_fmt = "%b %d, %Y"
 html_theme = 'sphinx_wagtail_theme'
 html_theme_options = {
     'project_name': "Crazy Development",
-    'logo': "/assets/logo.png",
+    'logo': "https://cdn.crazydev.org/_data/i/upload/2024/08/29/20240829135605-1e4ae887-th.png",
     'github_url': "https://github.com/Crazy-Development/crazydev-docs",
     'logo_alt': "crazydev",
     'logo_height': 59,
@@ -49,6 +50,11 @@ html_theme_options = {
     'logo_url': "/",
     'header_links': "Crazydev - Blog |http://crazydev.org", }
 
+html_context = {
+"display_github": False,
+"last_updated": True,
+"commit": False,
+}
 
 # -- Options for EPUB output -------------------------------------------------
 epub_show_urls = 'footnote'
