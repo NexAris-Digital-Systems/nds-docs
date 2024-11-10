aa59b4f2f11761da7571d1dd91a2c78a97c512d9
MrRamyg <153380674+MrRamyg@users.noreply.github.com>
2024-11-10 16:33:39 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,5 @@
 # -- Project information -----------------------------------------------------
-project = 'Crazy Development'
+project = 'NexAris Digital Systems'
 author = 'MrRamyg'
 release = '0.1'
 version = '0.1.0'
@@ -7,7 +7,7 @@ version = '0.1.0'
 # -- General configuration ---------------------------------------------------
 html_show_sourcelink = False
 
-html_favicon = "https://cdn.crazydev.org/_data/i/upload/2024/08/14/20240814080422-a2794fa4-th.png"
+html_favicon = "https://images.nexarisds.org/evidence/67281bb3775a1.jpg"
 extensions = [
     'myst_parser',
     'sphinx.ext.autodoc',
@@ -43,14 +43,13 @@ html_last_updated_fmt = "%b %d, %Y"
 
 html_theme = 'sphinx_wagtail_theme'
 html_theme_options = {
-    'project_name': "Crazy Development",
+    'project_name': "NexAris Digital Systems",
     'logo': "https://cdn.crazydev.org/_data/i/upload/2024/08/29/20240829135605-1e4ae887-th.png",
-    'github_url': "https://github.com/Crazy-Development/crazydev-docs",
-    'logo_alt': "crazydev",
+    'github_url': "https://github.com/NexAris-Digital-Systems/nds-docs/",
+    'logo_alt': "nds",
     'logo_height': 59,
     'logo_width': 45,
-    'logo_url': "/",
-    'header_links': "Crazydev - Blog |http://crazydev.org", }
+    'logo_url': "/",}
 
 html_context = {
     "display_github": False,
